8c6bffad164c0d6174e0935ad4f554f9cb21aca1
Juan Sebastian Obando Mora <juansebastianobandomora@gmail.com>
2022-03-26 16:54:28 -0500

    Agregado input reader version alpha

---
+++ InputReader.py
@@ -0,0 +1,16 @@
+# Input reader (alpha 0.011)
+
+import tkinter
+from tkinter import font
+
+newWindow = tkinter.Tk()
+newWindow.geometry("150x150")
+newWindow.maxsize(300, 300)
+
+titleLabel = tkinter.Label(newWindow, text="Input Reader", font="sourcesans")
+titleLabel.pack()
+
+inputReader = tkinter.Entry(newWindow)
+inputReader.pack()
+
+newWindow.mainloop()
